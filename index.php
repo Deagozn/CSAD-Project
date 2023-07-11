134382245fb9d589a581c868bc0ead011039118a
Deagozn <124474840+Deagozn@users.noreply.github.com>
2023-07-11 21:23:36 +0800

    init

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <div class="navbar">
+            <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar-container">
+                <div class="container">
+                    <div class="navbar-wrapper">
+                        <a href="#" class="navbar-brand"><img src="Resources/logo-no-background.png" class="logo" loading="lazy" alt=""></a>
+                        <nav role="navigation" class="nav-menu-wrapper">
+                            <div class="nav-menu">
+                                <ul role="list" class="nav-menu-list">
+                                    <li>
+                                        <a href="#" class="nav-link">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="nav-link">Bookings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="nav-link">Library Finder</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="nav-link">Feedback</a>
+                                    </li>
+                                </ul>
+                                <ul role="list" class="nav-menu-list">
+                                    <li class="mobile-margin-top-10">
+                                        <a href="#" class="login">LOGIN</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <div class="menu-button"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="page">
+            <div class="box-container"></div>
+        </div>
+    </body>
+</html>
