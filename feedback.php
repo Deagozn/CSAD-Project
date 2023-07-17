d16ab9ec91184c96eedfef1d9509cefabf70edde
Deagozn <Deagozn@users.noreply.github.com>
2023-07-17 21:43:40 +0800

    added feedback form with image upload

--- feedback.php
+++ feedback.php
@@ -42,10 +42,61 @@
     </div>  
   </div>
 </nav>
-<div>
+<div class="flex items-center justify-center h-screen">
+    <div class="max-w-md w-full mx-4 bg-white rounded-lg border border-black shadow-lg p-8">
+    <h1 class="text-3xl font-semibold mb-6">Feedback</h1>
+    <form>
+      <div class="mb-6">
+        <label for="feedback_type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Feedback Type</label>
+        <select id="feedback_type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+          <option value="BUG" selected>Bugs</option>
+          <option value="FEEDBACK">Feedback</option>
+          <option value="OTHERS">Others</option>
+        </select>
+      </div>
+      <div class="mb-6">
+        <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+        <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write a breif descrition of your problem/feedback here" required></textarea>
+      </div>
+        <div class="mb-6">     
+            <div class="flex items-center justify-center w-full">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                  <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                    </svg>
+                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400 text-center"><span class="font-semibold">Click to upload </span><span class="font-bold">one</span> image to support your feedback/report</p>
+                    <p class="text-xs text-gray-500 dark:text-gray-400">PNG or JPG (MAX. 800x400px)</p>
+                  </div>
+                  <input id="dropzone-file" type="file" class="hidden" onchange="displayFileNames(this)" multiple />
+                  <p id="file-names" class="text-sm text-gray-600 mt-2"></p>
+                </label>
+            </div>
+        </div>
+        
+      <button type="submit" class="w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-3 rounded">SUBMIT</button>
+    </form>
+  </div>
     
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
+<script>
+   function displayFileNames(input) {
+    const fileNames = document.getElementById('file-names');
+    fileNames.textContent = '';
+
+    for (let i = 0; i < input.files.length; i++) {
+      const fileName = input.files[i].name;
+      const span = document.createElement('span');
+      span.textContent = fileName;
+      fileNames.appendChild(span);
+
+      if (i !== input.files.length - 1) {
+        fileNames.appendChild(document.createTextNode(', '));
+      }
+    }
+  }
+</script>
 </body>
 </html>
