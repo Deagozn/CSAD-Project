781942c7753562df778a3b9650dd74c8375d9fc1
Chester <Chestern2121@gmail.com>
2023-07-19 08:29:21 +0800

    SQL create account

---
+++ forgot_passwordpt2.php
@@ -0,0 +1,30 @@
+<?php require_once "controller.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Tailwind CSS CDN link -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
+</head>
+<body class="bg-auto bg-repeat flex items-center justify-center h-screen" style="background-image: url('Resources/book-background.png')">
+  
+  <div class="max-w-md w-full mx-4 bg-white rounded-lg border border-black shadow-lg p-8">
+    <img src="Resources/logo-no-background.png" class="h-20 mr-5" alt="Kiasu Library Logo" style="margin-bottom: 30px; margin-left: 30%; width: 50%; height: auto"/>
+    <h1 class="text-3xl font-semibold mb-6 items-center">Forgot Password</h1>
+    <h3 class="text-l mb-10">Please Enter Email for Verification</h3>
+    <form>
+      <div class="mb-6">
+        <label for="email" class="block text-gray-700 font-medium">Email</label>
+        <input type="email" id="email" class="form-input mt-1 block w-full" placeholder="Enter your email" required>
+      </div>
+      <button type="submit" class="w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-3 rounded">VERIFY</button>
+    </form>
+    <div class="mt-4 text-center">
+      <a href="login.php" class="text-blue-500 hover:text-blue-700 text-sm underline">BACK TO LOGIN</a>
+    </div>
+  </div>
+
+</body>
+</html>
--- userotp.php
+++ userotp.php
@@ -1,4 +1,10 @@
 <?php require_once "controller.php";?>
+<?php 
+$email = $_SESSION['email'];
+if($email == false){
+  header('Location: login.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,14 +13,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Tailwind CSS CDN link -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
+  <link a href="style.css" rel="stylesheet">
 </head>
 <body class="bg-auto bg-repeat flex items-center justify-center h-screen" style="background-image: url('Resources/book-background.png')">
   
   <div class="max-w-md w-full mx-4 bg-white rounded-lg border border-black shadow-lg p-8">
     <img src="Resources/logo-no-background.png" class="h-20 mr-5" alt="Kiasu Library Logo" style="margin-bottom: 30px; margin-left: 30%; width: 50%; height: auto"/>
-    <h1 class="text-3xl font-semibold mb-6">Welcome Back :)</h1>
-    <form action="login.php" method="POST" autocomplete="">
-         <?php
+    <form action="userotp.php" method="POST" autocomplete="off">
+                    <h2 class="text-3xl font-semibold mb-6">Code Verification</h2>
+                    <?php 
+                    if(isset($_SESSION['info'])){
+                        ?>
+                        <div class="alert alert-success text-center">
+                            <?php echo $_SESSION['info']; ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <?php
                     if(count($errors) > 0){
                         ?>
                         <div class="alert alert-danger text-center">
@@ -27,22 +43,17 @@
                         <?php
                     }
                     ?>
-      <div class="mb-6">
-        <label for="email" class="block text-gray-700 font-medium">Email</label>
-        <input type="email" id="email" class="form-input mt-1 block w-full" placeholder="Enter your email" required>
-      </div>
-      <div class="mb-6">
-        <label for="password" class="block text-gray-700 font-medium">Password</label>
-        <input type="password" id="password" class="form-input mt-1 block w-full" placeholder="Enter your password" required>
-      </div>
-      <button type="submit" class="w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-3 rounded">LOGIN</button>
-    </form>
-    <div class="mt-4 text-center">
-      <a href="create_account.php" class="text-blue-500 hover:text-blue-700 text-sm">Create Account</a>
-      <span class="text-gray-500 mx-2">|</span>
-      <a href="forgot_passwordpt1.php" class="text-blue-500 hover:text-blue-700 text-sm">Forgot Password</a>
-    </div>
+                    <div class="form-group">
+                        <input class="form-control mt-1 block w-full" type="number" name="otp" placeholder="Enter verification code" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-3 rounded mt-2" type="submit" name="check" value="Submit">
+                    </div>
+                </form>
+           
   </div>
-
 </body>
 </html>
+
+
+
