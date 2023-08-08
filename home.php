42c1acb78c668d7527d7473f8573d30853f70772
Deagozn <124474840+Deagozn@users.noreply.github.com>
2023-08-08 12:17:47 +0800

    stuff

--- home.php
+++ home.php
@@ -42,7 +42,7 @@
           <div class="dropdown">
           <button class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" >LIBRARY FINDER</button>
             <div class="dropdown-content">
-            <a href="library_finder.php">Locator</a>
+            <a href="library_finder.php">Libraries</a>
             <a href="Library_Directions.php">Directions</a>
             </div>
           </div>        
@@ -58,8 +58,16 @@
   </div>
 </nav>
 
-<div>
+<div class="flex justify-center items-center h-60vh">
     
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="Resources/image.png" alt="">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</a>
+
 </div>
 <div class="flex justify-center items-center h-60vh">
   <!-- Image container with relative position -->
